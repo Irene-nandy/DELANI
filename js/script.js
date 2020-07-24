5d16facebd71d4413a21d1f59da407f2eba045d7
Irene-nandy <irenekasiva75@gmail.com>
2020-07-24 14:47:08 +0300

    modify changes

--- js/script.js
+++ js/script.js
@@ -90,7 +90,7 @@ $(document).ready(function(){
       var email = $("input#MERGE0").val();
       var message = $("textarea#comment").val();
       if ($("input#MERGE1").val() && $("input#MERGE0").val()){
-        alert (name + ", we have received your message. Thank you for reaching out to us.");
+        alert (name + ", your messeges has been received successfull. Thank you for reaching out to us out.");
       }
       else {
         alert("Please enter your name and email!");
